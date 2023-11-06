b684364893c6da8fb508accf79c6784a6c8ea31b
Diego Aguilera <dsaguileraj@gmail.com>
2023-11-06 11:27:55 -0500

    Version Inicial: Login y Data bases

---
+++ home/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except:
+                return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'Usuario ya creado'})
+        return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'Las contraseñas no coinciden'})
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {'form': AuthenticationForm, 'error': 'Credenciales incorrectas'})
+        else:
+            login(request, user)
+            return redirect('tasks')
